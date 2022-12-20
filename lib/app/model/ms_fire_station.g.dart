f0507d402b7da45d92b28ac9662beebbf408d962
TruongNV <developer.nguyentruong@gmail.com>
2022-12-20 21:44:21 +0700

    Add generator file ms_fire_station

---
+++ lib/app/model/ms_fire_station.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ms_fire_station.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MSFireStation _$MSFireStationFromJson(Map<String, dynamic> json) =>
+    MSFireStation(
+      FireStationCD: json['FireStationCD'],
+      Name: json['Name'],
+      Address: json['Address'],
+      TEL: json['TEL'],
+    );
+
+Map<String, dynamic> _$MSFireStationToJson(MSFireStation instance) =>
+    <String, dynamic>{
+      'FireStationCD': instance.FireStationCD,
+      'Name': instance.Name,
+      'Address': instance.Address,
+      'TEL': instance.TEL,
+    };
