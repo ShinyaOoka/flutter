5c87f9b9f2c3811ad46a2dc8f9cd65a56010b745
TruongNV <developer.nguyentruong@gmail.com>
2023-01-11 15:21:54 +0700

    Fix bug UI item list report works incorrect

--- lib/app/view/list_report/item_report.dart
+++ lib/app/view/list_report/item_report.dart
@@ -1,16 +1,15 @@
 import 'package:ak_azm_flutter/app/model/dt_report.dart';
-import 'package:ak_azm_flutter/app/module/common/extension.dart';
+import 'package:collection/collection.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 import '../../../generated/locale_keys.g.dart';
 import '../../model/ms_classification.dart';
 import '../../module/res/style.dart';
-import 'package:collection/collection.dart';
 
 class ItemReport extends StatelessWidget {
   DTReport report;
-  List<MSClassification>  msClassifications;
+  List<MSClassification> msClassifications;
   VoidCallback onClickItem;
   VoidCallback onDeleteItem;
 
@@ -28,7 +27,7 @@ class ItemReport extends StatelessWidget {
       child: InkWell(
         onTap: () => onClickItem.call(),
         child: Container(
-          padding: EdgeInsets.symmetric(vertical: size_10_w, horizontal: size_16_w),
+          padding: EdgeInsets.symmetric(vertical: size_10_w, horizontal: size_10_w),
           child: Row(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -44,56 +43,40 @@ class ItemReport extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(
-                          '${report.DateOfOccurrence ?? ''} ${report.TimeOfOccurrence ?? ''}',
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                              overflow: TextOverflow.ellipsis,
-                              color: Colors.black,
-                              fontSize: text_14,
-                              fontWeight: FontWeight.normal),
+                        Expanded(
+                          child: Text(
+                            '${report.DateOfOccurrence ?? ''} ${report.TimeOfOccurrence ?? ''}',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(overflow: TextOverflow.ellipsis, color: Colors.black, fontSize: text_12, fontWeight: FontWeight.normal),
+                          ),
+                          flex: 1,
                         ),
-                        Text(
-                          LocaleKeys.corps_name.tr(
-                              namedArgs: {'corps_name': report.TeamName ?? ''}),
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                              overflow: TextOverflow.ellipsis,
-                              color: Colors.black,
-                              fontSize: text_14,
-                              fontWeight: FontWeight.normal),
+                        Expanded(
+                          child: Text(
+                            LocaleKeys.corps_name.tr(namedArgs: {'corps_name': report.TeamName ?? ''}),
+                            textAlign: TextAlign.start,
+                            style: TextStyle(overflow: TextOverflow.ellipsis, color: Colors.black, fontSize: text_12, fontWeight: FontWeight.normal),
+                          ),
+                          flex: 1,
                         ),
                       ],
                     ),
-
                     SizedBox(
                       height: size_3_w,
                     ),
                     Text(
-                      LocaleKeys.accident_type.tr(namedArgs: {
-                        'accident_type': finMSClassification()?.Value ?? '',
-                      }),
+                      LocaleKeys.accident_type.tr(namedArgs: {'accident_type': finMSClassification()?.Value ?? ''}),
                       textAlign: TextAlign.start,
-                      style: TextStyle(
-                          overflow: TextOverflow.ellipsis,
-                          color: Colors.black,
-                          fontSize: text_14,
-                          fontWeight: FontWeight.normal),
+                      style: TextStyle(overflow: TextOverflow.ellipsis, color: Colors.black, fontSize: text_12, fontWeight: FontWeight.normal),
                     ),
                     SizedBox(
                       height: size_3_w,
                     ),
                     Text(
-                      LocaleKeys.description.tr(namedArgs: {
-                        'description': report.AccidentSummary ?? '',
-                      }),
+                      LocaleKeys.description.tr(namedArgs: {'description': report.AccidentSummary ?? ''}),
                       textAlign: TextAlign.start,
                       maxLines: 2,
-                      style: TextStyle(
-                          overflow: TextOverflow.ellipsis,
-                          color: Colors.black,
-                          fontSize: text_14,
-                          fontWeight: FontWeight.normal),
+                      style: TextStyle(overflow: TextOverflow.ellipsis, color: Colors.black, fontSize: text_12, fontWeight: FontWeight.normal),
                     ),
                   ],
                 ),
@@ -105,8 +88,8 @@ class ItemReport extends StatelessWidget {
     );
   }
 
-  MSClassification? finMSClassification(){
-    if(msClassifications.isEmpty) return null;
+  MSClassification? finMSClassification() {
+    if (msClassifications.isEmpty) return null;
     return msClassifications.firstWhereOrNull((element) => report.TypeOfAccident == element.ClassificationSubCD && element.ClassificationCD == '002');
   }
 }
