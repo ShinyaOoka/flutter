53377d33a84ebf2d96689982a5ea960143328646
bachdang-theinfitech <94923949+bachdang-theinfitech@users.noreply.github.com>
2023-02-01 08:51:22 +0700

    remove unused files

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:ak_azm_flutter/di/components/service_locator.dart';
-import 'package:ak_azm_flutter/flavors.dart';
 import 'package:ak_azm_flutter/pigeon.dart';
 import 'package:ak_azm_flutter/ui/my_app.dart';
 import 'package:ak_azm_flutter/utils/zoll_sdk_flutter_api_impl.dart';
 
 Future<void> main() async {
-  F.appFlavor = Flavor.DEVELOPMENT;
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
   return runZonedGuarded(() async {
