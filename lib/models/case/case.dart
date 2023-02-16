22be77b3c48997d99410a6273c2568b8130785c6
bachdang-theinfitech <94923949+bachdang-theinfitech@users.noreply.github.com>
2023-02-16 16:04:10 +0700

    Change event filter

--- lib/models/case/case.dart
+++ lib/models/case/case.dart
@@ -45,12 +45,12 @@ abstract class _Case with Store {
           return e.item2.type == "Aed" ||
               e.item2.type == "AlarmLimits" ||
               e.item2.type == "DataChannels" ||
-              e.item2.type == "DefibTrace" ||
+              // e.item2.type == "DefibTrace" ||
               e.item2.type == "DeviceConfiguration" ||
               e.item2.type == "DisplayInfo" ||
               e.item2.type == "NewCase" ||
               e.item2.type == "PatientInfo" ||
-              e.item2.type == "PrtTrace" ||
+              // e.item2.type == "PrtTrace" ||
               e.item2.type == "SnapshotRpt" ||
               e.item2.type == "SysLogEntry" ||
               e.item2.type == "TreatmentSnapshotEvt" ||
