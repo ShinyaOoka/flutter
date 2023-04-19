f6a0faee32a50199bdcf4d8aab12111b8a342f64
bachdang-theinfitech <94923949+bachdang-theinfitech@users.noreply.github.com>
2023-04-19 11:44:09 +0700

    Add snapshot text

--- lib/ui/data_viewer/snapshot_detail_screen/snapshot_detail_screen.dart
+++ lib/ui/data_viewer/snapshot_detail_screen/snapshot_detail_screen.dart
@@ -103,6 +103,40 @@ class _SnapshotDetailScreenState extends State<SnapshotDetailScreen>
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
+            padding: EdgeInsets.all(16),
+            child: Wrap(
+              spacing: 8,
+              children: [
+                Text(
+                    "| Etco2 ${snapshot.trend.etco2.value} ${snapshot.trend.etco2.unit}"),
+                Text(
+                    "| Fico2 ${snapshot.trend.fico2.value} ${snapshot.trend.fico2.unit}"),
+                Text("HR ${snapshot.trend.hr.value} ${snapshot.trend.hr.unit}"),
+                Text(
+                    "| nibpDia ${snapshot.trend.nibpDia.value} ${snapshot.trend.nibpDia.unit}"),
+                Text(
+                    "| nibpMap ${snapshot.trend.nibpMap.value} ${snapshot.trend.nibpMap.unit}"),
+                Text(
+                    "| nibpSys ${snapshot.trend.nibpSys.value} ${snapshot.trend.nibpSys.unit}"),
+                Text("PI ${snapshot.trend.pI.value} ${snapshot.trend.pI.unit}"),
+                Text(
+                    "| PVI ${snapshot.trend.pVI.value} ${snapshot.trend.pVI.unit}"),
+                Text(
+                    "| resp ${snapshot.trend.resp.value} ${snapshot.trend.resp.unit}"),
+                Text(
+                    "| spCo ${snapshot.trend.spCo.value} ${snapshot.trend.spCo.unit}"),
+                Text(
+                    "| spHb ${snapshot.trend.spHb.value} ${snapshot.trend.spHb.unit}"),
+                Text(
+                    "| spMet ${snapshot.trend.spMet.value} ${snapshot.trend.spMet.unit}"),
+                Text(
+                    "| spOC ${snapshot.trend.spOC.value} ${snapshot.trend.spOC.unit}"),
+                Text(
+                    "| spo2 ${snapshot.trend.spo2.value} ${snapshot.trend.spo2.unit}"),
+              ],
+            ),
+          ),
+          Container(
             child: Text('Pads'),
             padding: EdgeInsets.all(16),
           ),
