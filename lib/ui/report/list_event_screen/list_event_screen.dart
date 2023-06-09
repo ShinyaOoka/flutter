437b7f4c8ff225de50b0cb80337a77eabae467a0
Bach Dang <bach_dang@theinfitech.com>
2023-06-09 14:36:39 +0700

    update

--- lib/ui/report/list_event_screen/list_event_screen.dart
+++ lib/ui/report/list_event_screen/list_event_screen.dart
@@ -170,22 +170,27 @@ class _ListEventScreenState extends State<ListEventScreen>
     });
 
     final tempDir = await getTemporaryDirectory();
-    await File('${tempDir.path}/demo.json')
-        .writeAsString(await rootBundle.loadString("assets/example/demo.json"));
+    await _loadTestData();
+    _hostApi.deviceDownloadCase(
+        _zollSdkStore.selectedDevice!, caseId, tempDir.path, null);
+  }
+
+  Future<void> _loadTestData() async {
+    final tempDir = await getTemporaryDirectory();
+    await File('${tempDir.path}/$caseId.json').writeAsString(
+        await rootBundle.loadString("assets/example/$caseId.json"));
     final caseListItem = _zollSdkStore
         .caseListItems[_zollSdkStore.selectedDevice?.serialNumber]
         ?.firstWhere((element) => element.caseId == caseId);
     final parsedCase = CaseParser.parse(
-        await rootBundle.loadString("assets/example/demo.json"));
-    _zollSdkStore.cases['caseId'] = parsedCase;
+        await rootBundle.loadString("assets/example/$caseId.json"));
+    _zollSdkStore.cases[caseId] = parsedCase;
     parsedCase.startTime = caseListItem?.startTime != null
         ? DateTime.parse(caseListItem!.startTime!).toLocal()
         : null;
     parsedCase.endTime = caseListItem?.endTime != null
         ? DateTime.parse(caseListItem!.endTime!).toLocal()
         : null;
-    _hostApi.deviceDownloadCase(
-        _zollSdkStore.selectedDevice!, caseId, tempDir.path, null);
   }
 
   @override
